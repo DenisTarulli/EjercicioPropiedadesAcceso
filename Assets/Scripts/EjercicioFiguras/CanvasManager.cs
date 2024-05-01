bd4ef9c7beadcce4904dbf27b7e2881be7eb6092
Denis Tarulli <132310271+DenisTarulli@users.noreply.github.com>
2024-05-01 13:05:32 -0300

    Initial commit

---
+++ Assets/Scripts/EjercicioFiguras/CanvasManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+
+    public void AnimacionMensaje(string trigger)
+    {
+        anim.SetTrigger(trigger);
+    }
+}
